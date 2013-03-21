6847e887ad5db92f078173440ce241ed9521bfdc
Theo Armour <t.armour@gmail.com>
2013-03-20 22:45:50 -0700

    Added Nextbus data. Updated permalinks

--- haxis/r4/sidebars-03-hash.js
+++ haxis/r4/sidebars-03-hash.js
@@ -87,14 +87,13 @@
 	function initText() {
 		sidebarLeft.innerHTML =
 			'<div class="control" onclick="toggleBar( sidebarLeft )">[X]</div>' +
-			'<h1>hAxis r4.1</h1>' +
+			'<h1>hAxis r4</h1>' +
 			'<p><i>2013-03-19 ~ still pre-release</i></p>' +
 			'<p>Made by the <a href="http://www.urdacha.info" target="_blank">Urdacha</a> team for the:</p>' +
 			'<p><a href="http://urbanprototyping.org/prototype/challenges/urban-data-challenge-zurich-sf-geneva/" target="_blank">Urban Data Challenge</a></p>' +
 			'<p>Watch the numeric data come alive as animated objects in 3D.</p>' +
 			'<p>Use your pointing device to update the view.</p>' +
-			'<p><b><i>Rotate</i></b>: Left button/1 finger down<br><b><i>Zoom</i></b>: Wheel/2 fingers<br>' +
-	// '<b><i>Pan</i></b>: Right button/2 fingers down</p>' +
+			'<p><b><i>Rotate</i></b>: Left button/1 finger down<br><b><i>Zoom</i></b>: Wheel/2 fingers<br><b><i>Pan</i></b>: Right button/2 fingers down</p>' +
 			'<p>hAxis = hackathon + Axis</p>' +
 			
 			'<p><a href="https://github.com/jaanga/urdacha/wiki/hAxis-Issues-and-Wish-List" target="_blank">Wish List</a><br>' +
@@ -115,7 +114,6 @@
 			'</div>';
 		HAX.updates = document.getElementById('toggled');
 
-
 		sidebarRight.innerHTML =
 			'<div class="control" onclick="toggleBar( sidebarRight )">[X]</div>' +
 			'<h2>Control Panel</h2>' +
@@ -124,15 +122,15 @@
 				'<h3>Load City Data</h3>' +
 				'For overview:<br><small>Click a link below. Prepare to wait 15/20 seconds for data to download from server.</small>' +
 				'<table style="margin: 0 0 10px 0" ><tr><td width="150px">' +
-				'<a href="#" onclick="doIt(1); " >SF Day 1</a><br>' + 
-				'<a href="#" onclick="doIt(2); " >SF Day 2</a><br>' + 
-				'<a href="#" onclick="doIt(3); " >SF Day 3</a><br>' + 
-				'<a href="#" onclick="doIt(4); " >SF Day 4</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day1.csv\'; loadSF_Day(); " >SF Day 1</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day2.csv\'; loadSF_Day(); " >SF Day 2</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day3.csv\'; loadSF_Day(); " >SF Day 3</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day4.csv\'; loadSF_Day(); " >SF Day 4</a><br>' + 
 				'</td><td valign="top">' +
-				'<a href="#" onclick="doIt(5); " >SF Day 5</a><br>' + 
-				'<a href="#" onclick="doIt(6); " >SF Day 6</a><br>' + 
-				'<a href="#" onclick="doIt(7); " >SF Day 7</a><br>' + 
-				'<a href="#" onclick="doIt(8); " >SF Day 71 ~ r vs s</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day5.csv\'; loadSF_Day(); " >SF Day 5</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day6.csv\'; loadSF_Day(); " >SF Day 6</a><br>' + 
+				'<a href="#" onclick="HAX.fname = \'sf_day7.csv\'; loadSF_Day(); " >SF Day 7</a><br>' + 
+				// '<a href="#" onclick="loadSF_Day( \'../../improved-csv/sf_day4.csv\'); " >SF Day 4</a><br>' + 
 				'</td></tr></table>' +				
 				/*
 				'For research:<div style="font-size: small; margin: 0 0 5px 0; padding: 0;">' +
@@ -155,73 +153,30 @@
 			toggleStatusBar(0);
 		}
 	}
-		function doIt( item ) {
-			if (item === 1) {
-				HAX.fname = 'sf_day1.csv';
-				HAX.title = 'San Francisco Day 1';
-			} else if (item === 2) {
-				HAX.fname = 'sf_day2.csv';
-				HAX.title = 'San Francisco Day 2';
-				HAX.bars = 0;
-			} else if (item === 3) {
-				HAX.fname = 'sf_day3.csv';
-				HAX.title = 'San Francisco Day 3';
-			} else if (item === 4) {
-				HAX.fname = 'sf_day4.csv';
-				HAX.title = 'San Francisco Day 4';
-			} else if (item === 5) {
-				HAX.fname = 'sf_day5.csv';
-				HAX.title = 'San Francisco Day 5';
-			} else if (item === 6) {
-				HAX.fname = 'sf_day6.csv';
-				HAX.title = 'San Francisco Day 6';
-			} else if (item === 7) {
-				HAX.fname = 'sf_day7.csv';
-				HAX.title = 'San Francisco Day 7';
-			} else if (item === 8) {
-				HAX.fname = '_CSVData_sf_realtime_vs_scheduled_day1.csv';
-				HAX.title = 'San Francisco Day 1 ~ realtime vs scheduled';
-				HAX.date = 2;
-				HAX.time = 15;
-				HAX.speed = 10;
-				HAX.direction = 11;
-				HAX.x = 13;
-				HAX.y = 6;
-				HAX.z = 14;
-				HAX.color = 6;
-				HAX.objCount = 100;
-				HAX.lineField = 7;
-				HAX.lineEnd = 7;
-				HAX.lineCount = 20;
-				HAX.lineLength = 2;
-			}
-			loadSF_Day();
-		}
-
+	
 	function updateSbr( fields, h) {
 	
 		var htm2 = '<h3>Player Manager</h3>' +
 			'<p><button onclick="togglePlay()" >Toggle Play/Pause</button></p>' +
-			'Replay Speed: &nbsp;<input title="number of lines to read per frame" style="width: 180px;" type="range" min="2" max="100" ' +
-				'onchange="HAX.speed=this.value; haxisIt();" step="1" value="' + HAX.speed + '" ><br>' +
-			'Field of View: <input title="higher number = wider view. May help you to see inside the matrix." ' +
-				'style="width: 180px;" type="range" min="10" max="150" onchange="setFov( this.value ); haxisIt();" step="1" value="' + HAX.camFov + '" ><br>' +
-			'Choose Record: <input title="Set the current data line" style="width: 180px;" type="range" min="0" max="' + dataLength + 
-				'" onchange="dataCount=this.value; haxisIt();" step="500" value="' + dataCount + '" ><br>' +
+			'Replay Speed: &nbsp;<input title="number of lines to read per frame" style="width: 180px;" type="range" min="2" max="100" onchange="HAX.cameraData.speed=this.value; haxisIt();" step="1" value="' + HAX.speed + '" ><br>' +
+			'Field of View: <input title="higher number = wider view. May help you to see inside the matrix." style="width: 180px;" type="range" min="10" max="150" onchange="setFov( this.value ); haxisIt();" step="1" value="' + HAX.camFov + '" ><br>' +
+			'Choose Record: <input title="Set the current data line" style="width: 180px;" type="range" min="0" max="' + dataLength + '" onchange="dataCount=this.value; haxisIt();" step="500" value="' + dataCount + '" ><br>' +
 
-			'<br><scan style="font-weight: 600; color: red;">X-axis: </scan><select onchange="HAX.x = this.options.selectedIndex; buildReport(); updateUrlHash(); " title="select the X-axis" >' +
-				buildSelect( fields, HAX.x ) +
-			'<br><scan style="font-weight: 600; color: green;">Y-axis: </scan><select onchange="HAX.y = this.options.selectedIndex; buildReport(); updateUrlHash(); " title="select the Y-axis" ><br>' +
-				buildSelect( fields, HAX.y ) +
-			'<br><scan style="font-weight: 600; color: blue;">Z-axis: </scan><select onchange="HAX.z = this.options.selectedIndex; buildReport();  updateUrlHash(); " title="select the Z-axis" ><br>' +
-				buildSelect( fields, HAX.z ) + '<br>' +
+			'<br><scan style="font-weight: 600; color: red;">X-axis: </scan><select onchange="HAX.x = this.options.selectedIndex; buildReport(); updateReadOut( \'x\', HAX.x); " title="select the X-axis" >' +
+			buildSelect( fields, HAX.x ) +
+			'<br><scan style="font-weight: 600; color: green;">Y-axis: </scan><select onchange="HAX.y = this.options.selectedIndex; buildReport(); updateReadOut( \'y\', HAX.y);" title="select the Y-axis" ><br>' +
+			buildSelect( fields, HAX.y ) +
+			'<br><scan style="font-weight: 600; color: blue;">Z-axis: </scan><select onchange="HAX.z = this.options.selectedIndex; buildReport(); updateReadOut( \'z\', HAX.z);" title="select the Z-axis" ><br>' +
+			buildSelect( fields, HAX.z ) + '<br>' +
 
-			'<br>Colors: &nbsp; &nbsp; <select onchange="HAX.color = this.options.selectedIndex; buildReport(); updateUrlHash(); " title="select the filed that the colors indicate" ><br>' +
-				buildSelect( fields, HAX.color ) + '<br>' +
-			'Lines: &nbsp; &nbsp; &nbsp;<select onchange="HAX.lineField = this.options.selectedIndex; buildReport(); updateUrlHash(); " title="select the field that lines connect" ><br>' +
-				buildSelect( fields, HAX.lineField ) + '<br>' +
-			'Disconnect: <select onchange="HAX.lineEnd = this.options.selectedIndex; buildReport(); updateUrlHash(); " title="select the field that causes lines to disconnect" ><br>' +
-				buildSelect( fields, HAX.lineEnd ) + '<br>' +
+			'<br>Colors: &nbsp; &nbsp; <select onchange="HAX.color = this.options.selectedIndex; buildReport(); updateReadOut( \'color\', HAX.color);" title="select the filed that the colors indicate" ><br>' +
+			buildSelect( fields, HAX.color ) + '<br>' +
+			'Lines: &nbsp; &nbsp; &nbsp;<select onchange="HAX.lineField = this.options.selectedIndex; buildReport(); updateReadOut( \'lineField\', HAX.lineField);" title="select the field that lines connect" ><br>' +
+			buildSelect( fields, HAX.lineField ) + '<br>' +
+			'Disconnect: <select onchange="HAX.lineSubSetField = this.options.selectedIndex; buildReport(); updateReadOut( \'lineSubSetField\', HAX.lineSubSetField);" title="select the field that causes lines to disconnect" ><br>' +
+			//sbrBody.innerHTML = htm2;			
+			buildSelect( fields, HAX.lineSubSetField ) + '<br><br>' +
+
 		'';
 		sbrBody.innerHTML = htm2;
 	}
